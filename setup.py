81816aa8e82af5bd31a6454e6a6d0855621a5727
Pratik Asarkar <praasark@cisco.com>
2025-02-03 20:14:40 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project has a test code to test out the connection with openai gpt model with a website summarization functionality',
+    author='Pratik',
+    license='MIT',
+)
